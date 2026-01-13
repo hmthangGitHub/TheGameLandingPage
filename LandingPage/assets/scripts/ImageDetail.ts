528469242c710710efc0125ec7e14459cea5463d
thang <hoangmanhthang795@gmail.com>
2026-01-13 22:06:40 +0800

    remove cc base site

--- LandingPage/assets/scripts/ImageDetail.ts
+++
@@ -1,8 +0,0 @@
-import { _decorator, Component, Sprite } from 'cc';
-const { ccclass, property } = _decorator;
-
-@ccclass('ImageDetail')
-export class ImageDetail extends Component {
-    @property(Sprite)
-    public sprite: Sprite | null = null;
-}
